5dcfc04d4bad4ff25516a7b458bc8d0c14349fab
eltonmargonato <41756807+eltonmargonato@users.noreply.github.com>
2020-02-08 15:01:57 -0300

    Update LabCicdApplication.java

--- src/main/java/fiap/devops/cicd/labcicd/LabCicdApplication.java
+++ src/main/java/fiap/devops/cicd/labcicd/LabCicdApplication.java
@@ -6,9 +6,15 @@ teste ggggggg
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.junit.*;
 
 @SpringBootApplication
 public class LabCicdApplication {
+	
+	@Test
+        public void test_method_1() {
+                System.out.println("@Test - test_method_1");
+        }
 
 	public static void main(String[] args) {
 		SpringApplication.run(LabCicdApplication.class, args);
